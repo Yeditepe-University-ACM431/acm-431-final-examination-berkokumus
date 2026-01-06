990adde579f0b893211e90f6368aaea998cadcda
berkokumus <berkokumus00@gmail.com>
2026-01-06 15:28:37 +0300

    Implement mutable state list for tasks
    Added a mutable state list for Task objects and a toggle function.

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -1,13 +1,28 @@
 package com.yeditepe.finalexam.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.yeditepe.finalexam.model.Task
 
 class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
+    private val _tasks = mutableStateListOf<Task>()
+
+    private fun mutableStateListOf() {
+        TODO("Not yet implemented")
+
+
+
+    }
+
+    val tasks: List<Task> = _tasks
+
+
     // Initially add at least 2 tasks
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        val task = _tasks.find { it.id == taskId }
+        task?.isCompleted = !task?.isCompleted!!
     }
 }
